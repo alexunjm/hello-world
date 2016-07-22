f6c340853558bc260a63c0d66c62b73ebb83cdbb
alexunjm <alexunjm@gmail.com>
2016-07-22 09:45:49 -0500

    Nuevo template y parámetros desde la clase routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -12,5 +12,15 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    //return view('welcome');
+    return 'hi there';
+});
+
+Route::get('about', function() {
+	//$people = [];
+	$people = ['Marlon', 'Santi', 'More','Alex'];
+	//return view('pages.about', ['people'=>$people]);
+	//return view('pages.about', compact('people'));
+	//return view('pages.about')->with('people',$people);
+	return view('pages.about')->withPeople($people);
 });
