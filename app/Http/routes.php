00fef45da2fb050a3f9dbee413ed7a631a54b318
alexunjm <alexunjm@gmail.com>
2016-07-22 10:16:49 -0500

    parametros a vista desde routes.php

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,10 +11,7 @@
 |
 */
 
-Route::get('/', function () {
-    //return view('welcome');
-    return 'hi there';
-});
+Route::get('/', 'PagesController@home');
 
 Route::get('about', function() {
 	//$people = [];
